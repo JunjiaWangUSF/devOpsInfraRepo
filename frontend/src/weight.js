3f7c45c3842f195a5bf5415c071fbd4051fa45e9
Junjia Wang <jwang268@dons.usfca.edu>
2025-02-28 14:00:20 -0500

    chaning color

--- frontend/src/weight.js
+++ frontend/src/weight.js
@@ -59,7 +59,7 @@ function WeightTracker() {
 
   return (
     <div>
-      <h2>Track Your Weight</h2>
+      <h2 style={{ color: "blue" }}>Track Your Weight</h2>
       <form onSubmit={handleSubmit}>
         <input
           type="text"
